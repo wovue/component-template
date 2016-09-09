121c613e57239f6d51b7904391401ba22cffc89d
wochap <gean.marroquin@gmail.com>
2016-09-08 19:27:43 -0500

    update entry name

--- template/build/webpack.prod.dist.conf.js
+++ template/build/webpack.prod.dist.conf.js
@@ -11,7 +11,7 @@ var env = process.env.NODE_ENV === 'testing'
 
 var webpackConfig = merge(baseWebpackConfig, {
   entry: {
-    index: './src/main.js'
+    {{ name }}: './src/main.js'
   },
   module: {
     loaders: utils.styleLoaders({ sourceMap: prodConfig.dist.productionSourceMap, extract: true })
